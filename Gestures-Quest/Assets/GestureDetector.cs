c2286f9c99eab06329ec2d158c94e6aeebe4f22d
Gary Robinson <gary.robinson2524@gmail.com>
2020-07-24 14:33:54 -0400

    Added Gesture Detector Script
    Added OVRBone list to gain access to finger bones. Also created a Gesture struct to persist the type of data I need for the gestures

---
+++ Gestures-Quest/Assets/GestureDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//struct = class without function
+[System.Serializable]
+public struct Gesture
+{
+    public string name;
+    public List<Vector3> fingerDatas;
+    public UnityEvent onRecognized;
+}
+
+public class GestureDetector : MonoBehaviour
+{
+
+    public OVRSkeleton skeleton;
+    public List<Gesture> gestures;
+    private List<OVRBone> fingerBones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fingerBones = new List<OVRBone>(skeleton.Bones);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
